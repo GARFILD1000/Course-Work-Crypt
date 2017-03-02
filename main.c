80ce7dc3036df433d9d53be9040213f8531eb90e
Булгаков Дмитрий Олегович <GARFILD1000@rambler.ru>
2017-03-02 11:09:51 +0700

    Second commit

--- main.c
+++ main.c
@@ -2,33 +2,38 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int main()
-{
+int proverka(float srok,float sum){
+    if ((sum < 10000) || (srok < 0 || srok > 365)) {
+    return 1;
+    }
+    else return 0;
+}
+
+int main() {
 float srok, sum;
-    setlocale(LC_ALL,"rus");
-    printf("Vvedyte srok vklada ");
-    scanf("%f",&srok);
-    printf("Vvedyte summu vklada ");
-    scanf("%f",&sum);
-    do{
-        printf("Neverno vvedeny dannye, vvedyte zanovo \n");
-        printf("Vvedyte srok vklada ");
-        scanf("%f",&srok);
-        printf("Vvedyte summu vklada ");
-        scanf("%f",&sum);
-    }while((sum < 10000)||(srok < 0 || srok > 365));
+int check;
+    setlocale (LC_ALL, "rus");
+    do {
+        printf ("Введите cумму вклада ");
+        scanf ("%f", &sum);
+        printf ("Введите срок вклада ");
+        scanf ("%f", &srok);
+        check=proverka(srok,sum);
+        if (check == 1) {
+            printf ("Неверно введены данные, введите заново:\n");
+        };
+    } while (check);
     if (srok < 31) {
         sum-=sum/10;
     };
-    if ((srok >= 31)&&(srok<121)) {
+    if ((srok >= 31) && (srok < 121)) {
         sum+=sum/50;
     };
-    if ((srok >= 121)&&(srok<241)) {
+    if ((srok >= 121) && (srok < 241)) {
         sum+=(sum/100)*6;
     };
     if (srok >= 241) {
         sum+=(sum/100)*12;
     };
-    printf("Сумма вклада на момент окончания срока \n%.0f",sum); 
+    printf("Сумма вклада на момент окончания срока: \n%.0f\n ", sum); 
 }
-
