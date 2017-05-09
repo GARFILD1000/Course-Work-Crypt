0cfc7d80320b26508696612d981c93725ba5bf01
Dmitriy <GARFILD1000@rambler.ru>
2017-05-09 19:17:03 +0700

    Added restore window

--- source/SaveRestore.cpp
+++ source/SaveRestore.cpp
@@ -1,4 +1,5 @@
 void RestoreOptions();
+void RestoreOptionsWindow();
 
 //   процедура заполняет структуры с натройками изначальными значениями
 void RestoreOptions(){
@@ -25,4 +26,35 @@ void RestoreOptions(){
     options.input_file_name="InputFile.txt";
     options.output_file_name="OutputFile.txt";
     options.file_directory="..\\files\\";
+}
+
+void RestoreOptionsWindow(){
+    int point=1, button; 
+    do{ cleardevice();
+        outtextxy(300,70,"СБРОСИТЬ");
+        outtextxy(300,100,"НАСТРОЙКИ?");
+        setcolor(RGB(punkt_color.red,punkt_color.green,punkt_color.blue));
+        if (point==1) {
+            outtextxy(300,250,"настройки и статистика");
+            setcolor(RGB(negative_color.red,negative_color.green,negative_color.blue));
+            outtextxy(300,280,"сбросятся");
+            setcolor(RGB(point_color.red,point_color.green,point_color.blue));
+        };
+        outtextxy(300,160,"Да");
+        setcolor(RGB(punkt_color.red,punkt_color.green,punkt_color.blue));
+        if (point==2) {
+            
+            outtextxy(300,250,"настройки и статистика");
+            outtextxy(300,280,"не сбросятся");
+            setcolor(RGB(point_color.red,point_color.green,point_color.blue));
+        };
+        outtextxy(300,190,"Нет");
+        setcolor(RGB(punkt_color.red,punkt_color.green,punkt_color.blue));
+        button = getch();
+        switch(button){
+        case 72: if (point>1) point--; break;
+        case 80: if (point<2) point++; break;
+        case 13: RestoreOptions(); return;
+        };
+    }while(1);                                           
 }
